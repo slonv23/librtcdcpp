c6364b2e8c6f1d80f571a9ff64e7d730ff9a50d2
Illia Solonar <illia.solonar@gmail.com>
2020-01-27 23:39:55 +0200

    Add spdlog as submodule

--- cmake/Modules/FindSpdlog.cmake
+++ cmake/Modules/FindSpdlog.cmake
@@ -1,6 +1,6 @@
 if (NOT TARGET Gabime::Spdlog)
     include(FindPackageHandleStandardArgs)
-    find_path(SPDLOG_INCLUDE_DIR NAMES spdlog/spdlog.h PATHS ${CMAKE_SOURCE_DIR}/include)
+    find_path(SPDLOG_INCLUDE_DIR NAMES spdlog/spdlog.h PATHS ${CMAKE_SOURCE_DIR}/external/spdlog/include)
     find_package_handle_standard_args(Spdlog DEFAULT_MSG SPDLOG_INCLUDE_DIR)
     add_library(spdlog INTERFACE)
     target_include_directories(spdlog INTERFACE ${SPDLOG_INCLUDE_DIR})
